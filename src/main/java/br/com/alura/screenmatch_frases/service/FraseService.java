dbd9baca6617009687d56155dd52228634cbc4b7
Adriel Renato <adriel.renato26@gmail.com>
2025-03-15 16:47:49 -0300

    Implementação de busca de um registro aleatorio no banco!

--- src/main/java/br/com/alura/screenmatch_frases/service/FraseService.java
+++ src/main/java/br/com/alura/screenmatch_frases/service/FraseService.java
@@ -3,13 +3,15 @@ package br.com.alura.screenmatch_frases.service;
 import br.com.alura.screenmatch_frases.dto.FraseDTO;
 import br.com.alura.screenmatch_frases.repository.FraseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class FraseService {
     @Autowired
     FraseRepository repositorio;
 
     public FraseDTO obterFraseAleatoria() {
-        var frase = repositorio.findById("").get();
+        var frase = repositorio.buscaFraseAleatoria();
 
         return new FraseDTO(frase.getTitulo(), frase.getFrase(), frase.getPersonagem(), frase.getPoster());
     }
